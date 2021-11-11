f67f04104ab61f9e3d76391c4de84469772344fe
muhamedyoussry <mohamed.ahmed973@eng-st.cu.edu.eg>
2021-11-11 12:24:38 +0200

    adding plus minus

---
+++ Hacker Rank Problems/Equal Stacks.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2, n3 = 0;
+    cin >> n1, n2, n3;
+
+    
+    for (int i = 0; i < n1; i++)
+    {
+        
+    }
+    
+}
